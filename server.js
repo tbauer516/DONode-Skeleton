a4c0beed86b3118c79308120aed92d1c078d5f5d
tbauer516 <tcbauer516@gmail.com>
2016-11-18 15:44:28 -0800

    initial move of code to new repo

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var compression = require('compression');
+var app = express();
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/portfolio');
+
+var oneDay = 86400000;
+
+app.use(compression());
+
+app.use('/', express.static(__dirname + '/public', {maxAge: oneDay}));
+
+var port = 8108;//process.env.OPENSHIFT_NODEJS_PORT || 8000;
+
+app.listen(port, function() {//, ipadress, function() {
+  console.log(`Application worker ${process.pid} started...`);
+});
