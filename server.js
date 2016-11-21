8eba19ee7e0c2955a2100443da4965733ee3e269
tbauer516 <tcbauer516@gmail.com>
2016-11-21 14:34:49 -0800

    add test code from server

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+var fs = require('fs');
+var https = require('https');
 var express = require('express');
 var compression = require('compression');
 var app = express();
@@ -16,3 +18,11 @@ var port = 8108;//process.env.OPENSHIFT_NODEJS_PORT || 8000;
 app.listen(port, function() {//, ipadress, function() {
   console.log(`Application worker ${process.pid} started...`);
 });
+
+var privateKey = fs.readFileSync('/home/master/letsencrypt/config/live/tylerbauer.info/privkey.pem', 'utf8');
+var certificate = fs.readFileSync('/home/master/letsencrypt/config/live/tylerbauer.info/cert.pem', 'utf8');
+
+//https.createServer({
+//    key: privateKey,
+//    cert: certificate
+//}, app).listen(port);
